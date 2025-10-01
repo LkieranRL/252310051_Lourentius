ab29296e20601d6a32feecb677a3e81eadfe9fe3
LkieranRL <technoapp77@gmail.com>
2025-10-01 06:45:20 -0700

    Add files via upload

---
+++ tugas pr pert 2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+
+    cout << "Nomor 1 - Penjumlahan" << endl;
+    cout << "Masukkan 2 angka" << endl;
+    cout << "A = ";
+    cin >> a;
+    cout << "B = ";
+    cin >> b;
+    cout << "A + B = " << a + b << endl;
+    cout << "Hasil A + B = " << a + b << endl << endl;
+
+    cout << "Nomor 2 - Pengurangan" << endl;
+    cout << "Masukkan 2 angka" << endl;
+    cout << "A = ";
+    cin >> a;
+    cout << "B = ";
+    cin >> b;
+    cout << "A - B = " << a - b << endl;
+    cout << "Hasil A - B = " << a - b << endl << endl;
+
+    cout << "Nomor 3 - Perkalian" << endl;
+    cout << "Masukkan 2 angka" << endl;
+    cout << "A = ";
+    cin >> a;
+    cout << "B = ";
+    cin >> b;
+    cout << "A * B = " << a * b << endl;
+    cout << "Hasil A * B = " << a * b << endl;
+
+}
+
