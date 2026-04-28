5d0693f5111e3d2a28b07d84daf0adc5013ba6ba
LkieranRL <technoapp77@gmail.com>
2026-04-29 00:21:39 +0700

    Add files via upload

---
+++ Codingan LAB Analisis Algoritma dan Struktur Data/tugas 4 setter getter.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mahasiswa {
+private:
+    string nama;
+    string nim;
+    string jurusan;
+    int umur;
+
+public:
+    void setNama(string n) {
+        nama = n;
+    }
+
+    void setNIM(string n) {
+        nim = n;
+    }
+
+    void setJurusan(string j) {
+        jurusan = j;
+    }
+
+    void setUmur(int u) {
+        umur = u;
+    }
+
+    string getNama() {
+        return nama;
+    }
+
+    string getNIM() {
+        return nim;
+    }
+
+    string getJurusan() {
+        return jurusan;
+    }
+
+    int getUmur() {
+        return umur;
+    }
+};
+
+int main() {
+    Mahasiswa mhs;
+
+    string nama, nim, jurusan;
+    int umur;
+
+    cout << "=== Input Data Mahasiswa ===" << endl;
+
+    cout << "Nama	: ";
+    getline(cin, nama);
+
+    cout << "NIM    	: ";
+    getline(cin, nim);
+
+    cout << "Jurusan	: ";
+    getline(cin, jurusan);
+
+    cout << "Umur  	: ";
+    cin >> umur;
+
+    mhs.setNama(nama);
+    mhs.setNIM(nim);
+    mhs.setJurusan(jurusan);
+    mhs.setUmur(umur);
+
+    cout << "\n=== Data Mahasiswa ===" << endl;
+    cout << "Nama    : " << mhs.getNama() << endl;
+    cout << "NIM     : " << mhs.getNIM() << endl;
+    cout << "Jurusan : " << mhs.getJurusan() << endl;
+    cout << "Umur    : " << mhs.getUmur() << endl;
+
+    return 0;
+}
